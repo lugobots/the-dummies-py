ee2e57b3e13fe3b9e2f730a3a6483220664d2728
Ddarckwolf <aquersouza@gmail.com>
2023-07-04 19:06:45 -0300

    importing settings file and updating

--- main.py
+++ main.py
@@ -3,27 +3,14 @@ from lugo4py.client import NewClientFromConfig
 from lugo4py.mapper import Mapper
 from lugo4py.loader import EnvVarLoader
 from concurrent.futures import ThreadPoolExecutor
-
-PLAYER_POSITIONS = {
-    1: {'Col': 0, 'Row': 0},
-    2: {'Col': 1, 'Row': 1},
-    3: {'Col': 2, 'Row': 2},
-    4: {'Col': 2, 'Row': 3},
-    5: {'Col': 1, 'Row': 4},
-    6: {'Col': 3, 'Row': 1},
-    7: {'Col': 3, 'Row': 2},
-    8: {'Col': 3, 'Row': 3},
-    9: {'Col': 3, 'Row': 4},
-    10: {'Col': 4, 'Row': 3},
-    11: {'Col': 4, 'Row': 2},
-}
+from settings import PLAYER_POSITIONS, MAPPER_COLS, MAPPER_ROWS
 
 if __name__ == "__main__":
 
     config = EnvVarLoader()
 
     # The map will help us to see the field in quadrants (called regions) instead of working with coordinates
-    mapper = Mapper(10, 6, config.get_bot_team_side())
+    mapper = Mapper(MAPPER_COLS, MAPPER_ROWS, config.get_bot_team_side())
 
     # Our bot strategy defines our bot initial position based on its number
     initialRegion = mapper.get_region(PLAYER_POSITIONS[config.get_bot_number()]['Col'],
