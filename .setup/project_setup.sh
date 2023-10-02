279104b3d36b8897027160172935b9805f080a29
Angelo Katipunan <lugobots@gmail.com>
2023-10-02 11:24:03 -0600

    improving error message

--- .setup/project_setup.sh
+++ .setup/project_setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ -z "$(ls -A .)" ]; then
-   if ! git clone -q https://github.com/lugobots/the-dummies-py.git
+   if ! git clone -q https://github.com/lugobots/the-dummies-py.git  &>/dev/null
    then
      echo "fail to clone the repo :-("
      exit
@@ -15,14 +15,16 @@ if [ -z "$(ls -A .)" ]; then
    else
          INSTALL_VERSION=$VERSION
    fi
-   git fetch --all --tags -q
-   git checkout -q tags/$INSTALL_VERSION
+   git fetch --all --tags -q  &>/dev/null
+   git checkout -q tags/$INSTALL_VERSION || { echo 'could not checkout that tag, does it actually exist?' ; exit 1; }
    echo "Installing The Dummmies Py Verion "$INSTALL_VERSION
    cd ..
    mv the-dummies-py/* .
    rm -rf the-dummies-py
-   echo "All done! Please fix the file permissions running:
-   On Linux or Mac: chown x:x -R ."
+   echo "All done!"
+   echo ""
+   echo "On Linux or Mac, Please fix the file permissions running:"
+   echo "chown \$USER -R ."
 else
    echo "The output directory must be empty"
 fi
