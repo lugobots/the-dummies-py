3a92f09ba8da14be5a63a4ae2dedb176633ae1bb
Rubens Silva <rubens21@gmail.com>
2024-03-30 12:27:33 -0600

    adding log messages

--- .lugo/builder.sh
+++ .lugo/builder.sh
@@ -1,16 +1,20 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
+echo "Building bot image"
 cd .lugo || exit 1
-npm install -g nodemon@3.0.1
+echo " - Installing hot load watcher"
+npm install -g nodemon@3.0.1 > /dev/null
 
 cd /app || exit 2
 
 if [ ! -d ".lugo/venv" ]; then
+  echo " - Installing Python Environment"
   pip install virtualenv || exit 3
   virtualenv .lugo/venv --python=python3.9  || exit 4
+  chmod +xw -R .lugo/venv
 fi
 
-chmod +xw -R .lugo/venv
+echo " - Installing Python requirements"
 source .lugo/venv/bin/activate
-pip install -r requirements.txt
+pip install -q -r requirements.txt
 #chmod 777 -R .lugo/venv
